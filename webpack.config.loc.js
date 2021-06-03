a41928bdaa1284187e8087922eeb9eb0a454f9c9
Lais Frigério <laiss@gazetadopovo.com.br>
2021-06-03 16:09:23 -0300

    Add ESLint

--- webpack.config.loc.js
+++ webpack.config.loc.js
@@ -1,26 +1,26 @@
-const merge = require('webpack-merge');
-const baseConfig = require('./webpack.config.base');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const merge = require('webpack-merge')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const baseConfig = require('./webpack.config.base')
 
 module.exports = merge(baseConfig, {
-    mode: 'development',
+  mode: 'development',
 
-    module: {
-        rules: [
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
         ]
-    },
-
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css'
-        })
+      }
     ]
-});
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'
+    })
+  ]
+})
